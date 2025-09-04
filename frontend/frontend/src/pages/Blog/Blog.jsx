79476868ecbd553c8c91b8b6b2509f528c059a4d
Md. Amanullah Rijve <sheikhrijve885@gmail.com>
2025-09-04 19:31:42 +0600

    Add files via upload

---
+++ frontend/frontend/src/pages/Blog/Blog.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Blog() {
+  return (
+     <div className='bg-blue-300 pb-20'>
+            <div className='mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8 flex justify-center items-center' >
+                <div>
+                    <h1 className='font-semibold' >This page will be available soon</h1>
+                </div>
+            </div>
+        </div>
+  )
+}
+
+export default Blog
